87c275cde4653da75cdea694b9a94aa1664641db
Alan Jefferson Nascimento <alan.jefferson@outlook.com>
2022-04-25 20:44:47 -0300

    Add project to repository

---
+++ TapeEquilibrium/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapeEquilibrium
+{
+	public class Solution
+	{
+		public int solution(int[] A)
+		{
+			int value = 0;
+
+			return value;
+		}
+	}
+}
